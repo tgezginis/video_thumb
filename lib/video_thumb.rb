b97f70369dffb0f2ce6446ace099e2b69abc43b7
Adrian Peterson Co <adrianpco@gmail.com>
2017-03-28 16:44:05 +0800

    Whoops!

--- lib/video_thumb.rb
+++ lib/video_thumb.rb
@@ -39,8 +39,7 @@ module VideoThumb
     end
 
     if url.include? 'youtu.be' || url.include? 'youtube'
-      regex = 
-        /(https?:\/\/)?(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/
+      regex = /(https?:\/\/)?(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/
       url.gsub(regex) do
         youtube_video_id = $4
         image = "https://img.youtube.com/vi/#{youtube_video_id}/#{youtube_size}.jpg"
